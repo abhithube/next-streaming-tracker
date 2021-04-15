2ed2466cc7c70b1addcc247f423805d72adab7f2
Abhi Thube <abhimanyuthube@gmail.com>
2021-04-14 18:51:50 -0700

    Fixed root app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import axios from 'axios';
+import { AppProps } from 'next/app';
 import { Box, ChakraProvider, CSSReset } from '@chakra-ui/react';
 
 import Navbar from '../components/Navbar';
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 axios.defaults.params = { api_key: process.env.NEXT_PUBLIC_API_KEY };
-
-type AppProps = { Component: any; pageProps: any };
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
